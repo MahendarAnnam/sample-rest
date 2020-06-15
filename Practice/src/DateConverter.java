7734addd3b22388089642a775cfc80739266cda2
home <home@DESKTOP-6H7PQQC>
2020-06-15 12:02:37 +0530

    first commit

---
+++ Practice/src/DateConverter.java
@@ -0,0 +1,18 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class DateConverter {
+	public static void main(String[] arg) {
+	 
+		try {
+		   
+			Date date = new Date(1586508667*1000L);
+	        String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+	        TimeZone time = date.;
+		    System.out.println(format+"\n");
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+	}
+}
